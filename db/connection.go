1e355f74411321a327a53634f2df48258cc2721e
danshao <danshao@gmail.com>
2017-07-25 21:07:11 +0800

    cleanup db connection file

--- db/connection.go
+++ db/connection.go
@@ -11,24 +11,26 @@ import (
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
 
-var DBConnection *gorm.DB
-var once sync.Once
+var (
+	DBConnection *gorm.DB
+	once         sync.Once
+)
 
 // SharedConnection acts as a DB connection singleton
 func SharedConnection() *gorm.DB {
 	once.Do(func() {
 		var err error
-		DBConnection, err = gorm.Open("mysql", "root:root@tcp(127.0.0.1:3306)/pp")
 
+		DBConnection, err = gorm.Open("mysql", "root:root@tcp(mysql:3306)/pp")
 		if err != nil {
-			log.Println("Error connecting to MySQL DB: %v", err)
+			log.Println("Error connecting to MySQL DB: '%v'", err)
 		}
 
 		DBConnection.DB().SetMaxIdleConns(5)
 		DBConnection.DB().SetMaxOpenConns(50)
 		DBConnection.DB().SetConnMaxLifetime(30 * time.Second)
 
-		DBConnection.LogMode(false)
+		DBConnection.LogMode(true)
 
 		return
 	})
